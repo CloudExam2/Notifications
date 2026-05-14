51a535e3a478545bd47b11dec788255ca6a951cd
InakiMedina <inaki.medina@gmail.com>
2026-05-14 16:49:29 -0600

    fixed main.tf for the sqs

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "aws_sns_topic" "sales_notifications" {
   name = var.sns_topic_name
 }
 
-# 2. SNS Subscription (Email)
+# 2. SNS Subscription
 resource "aws_sns_topic_subscription" "email_target" {
   topic_arn = aws_sns_topic.sales_notifications.arn
   protocol  = "email"
@@ -19,10 +19,8 @@ resource "aws_sns_topic_subscription" "email_target" {
 }
 
 # 3. Lambda Function
-# Automatically gets your current Account ID and Partition
 data "aws_caller_identity" "current" {}
 
-# Dynamically constructs the LabRole ARN
 locals {
   lab_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 }
@@ -31,15 +29,7 @@ resource "aws_lambda_function" "notification_service" {
   function_name = var.lambda_function_name
   role          = local.lab_role_arn
   package_type  = "Image"
-  authorization_type = "NONE"
-  
-  # FIX: Using the data source directly is safer than constructing strings
   image_uri     = "${data.aws_ecr_repository.notification.repository_url}:latest"
-
-  cors {
-    allow_origins = ["*"]
-    allow_methods = ["POST"]
-  }
 
   environment {
     variables = {
@@ -48,16 +38,26 @@ resource "aws_lambda_function" "notification_service" {
   }
 }
 
-# 4. The trigger from SQS
+# 4. Lambda Function URL (Fixes Error: Unsupported argument/block)
+resource "aws_lambda_function_url" "notification_url" {
+  function_name      = aws_lambda_function.notification_service.function_name
+  authorization_type = "NONE"
 
+  cors {
+    allow_origins = ["*"]
+    allow_methods = ["POST"]
+  }
+}
+
+# 5. SQS Trigger (Fixes Error: Reference to undeclared resource)
 data "aws_sqs_queue" "ticket_queue" {
-  name = "sales-ticket-queue" # This MUST match the name in the AWS Console
+  name = "sales-ticket-queue"
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
-  # This uses the ARN discovered by the data block above
   event_source_arn = data.aws_sqs_queue.ticket_queue.arn
-  function_name    = aws_lambda_function.notification_lambda.arn
+  # Matches the resource name defined above
+  function_name    = aws_lambda_function.notification_service.arn
   enabled          = true
   batch_size       = 10
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,6 @@ output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
   value       = aws_sns_topic.sales_notifications.arn
 }
+
+output "lambda_function_url" {
+  value = aws_lambda_function_url.notification_url.function_url
